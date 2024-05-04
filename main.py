26fb32f9639df68b6736761ba79abf1c18aeeff1
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-05-04 09:59:50 +0100

    382. Step 4 - Creating and Adding to Spotify Playlist

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ response = requests.get(f'https://www.billboard.com/charts/hot-100/{date}')
 soup = BeautifulSoup(response.text, "html.parser")
 # print(soup)
 top_100 = [i.text.strip() for i in soup.select("li.o-chart-results-list__item h3.c-title")]
-print(top_100)
-
+playlist = []
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -23,5 +22,19 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
-print(user_id)
-
+year = date.split("-")[0]
+for song in top_100:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        playlist.append(result["tracks"]["items"][0]["uri"])
+        print(f"{song} added")
+    except IndexError:
+        print(f"{song} passed")
+        pass
+print(len(playlist))
+print(playlist)
+playlist_resp = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+playlist_id = playlist_resp['id']
+print(playlist_id)
+sp.playlist_add_items(playlist_id=playlist_id, items=playlist)
